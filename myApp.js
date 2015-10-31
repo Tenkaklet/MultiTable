8bc1a1b6160cafb91f134e926de027e244795539
Tenkaklet <tenkaklet@gmail.com>
2015-10-31 08:10:35 +0100

    second commit of follow along

--- myApp.js
+++ myApp.js
@@ -1,16 +1,13 @@
-angular.module('myApp', [])
-.controller('MultiplicationCtrl', function($scope, $attrs) {
-$scope.numberLimit = $attrs.initialNumberLimit || 10;
-
-$scope.numbers = function() {
-    var numbers = [];
-    for(var i=0; i<$scope.numberLimit; i++) {
-        numbers[i] = i + 1;
+angular.module('myApp', []).controller('MultiplicationCtrl', function ($scope, $attrs) {
+    $scope.numberLimit = $attrs.initialNumberLimit || 10;
+    $scope.numbers = function () {
+        var numbers = [];
+        for (var i = 0; i < $scope.numberLimit; i++) {
+            numbers[i] = i + 1;
+        }
+            return numbers;
     };
-    return numbers;
-};
-
-$scope.compute = function(a,b) {
-    return a * b;
-};
+    $scope.compute = function (a,b) {
+        return a * b;
+    };
 });
